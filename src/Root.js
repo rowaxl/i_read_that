719314ff8ad181634807716f875e864fc12ad31a
rowaxl <rowaxl0@gmail.com>
2019-07-24 22:12:31 +0900

    initialize project

---
+++ src/Root.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxPromise from 'redux-promise';
+import reducers from 'reducers';
+
+export default ({ children, initialState }) => {
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const store = createStore(
+        reducers,
+        initialState,
+        composeEnhancers(
+            applyMiddleware(reduxPromise)
+        )
+      );
+
+    return (
+        <Provider store={store}>
+            {children}
+        </Provider>
+    );
+};
